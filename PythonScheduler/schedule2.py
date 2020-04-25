dc41961ec8ae46a705e4627f9e575061c6b19ebf
Nkapoor19812 <55065941+Nkapoor19812@users.noreply.github.com>
2020-04-25 05:19:28 -0400

    alternatives added to output.xlsx

--- PythonScheduler/schedule2.py
+++ PythonScheduler/schedule2.py
@@ -1,15 +1,14 @@
 # File description: Scheduler that takes in classes and rooms and creates a schedule
-# Takes in 2 arguments: 1st arg - input xlsx file, 2nd arg - output csv file name
+# Takes in 2 arguments: 1st arg - input xlsx file, 2nd arg - output xlsx file name
 # testing
 import pandas as pd
 import xlrd
 import sys
+import xlsxwriter
 
 
 # Course object
 class Course(object):
-    # bool representing if course has been scheduled or not set to False by default
-    shed = False
 
     # Course constructor
     def __init__(self, subject, course, title, ver, sec, professor, time, cap):
@@ -21,10 +20,14 @@ class Course(object):
         self.professor = professor
         self.time = time
         self.cap = cap
+        # list of 3 possible alternatives
+        self.alt = []
+        # bool representing if course has been scheduled or not set to False by default
+        self.shed = False
 
     # course object print format
     def __repr__(self):
-        return self.subject + " " + str(self.course) + " " + self.professor + " " + str(self.cap) + " " + self.time
+        return self.subject + " " + str(self.course) + " " + self.professor + " " + str(self.cap)
 
 
 # room object
@@ -55,6 +58,81 @@ class Schedule(object):
 
     #  1 X 5 2D array that hold final solution each internal array representing a day of the week
     solution = [[], [], [], [], []]
+    unScheduled = []
+
+
+# generate_output: creates an xlsx output file that contains the information of the scheduled and unscheduled classes
+# Input: A Schedule object, a list of Course objects
+# Output: None
+def generate_output(schedule, courses):
+    # making a list that formats output info
+    output_list = []
+    alt_list = []
+
+    # save scheduled courses as strings
+    for solution in schedule.solution:
+        for i in solution:
+            for every in i.keys():
+                if str(every.ver) == 'nan':
+                    version = ""
+                else:
+                    version = every.ver
+                temp = [every.subject + " " + str(every.course), every.title, version, every.sec, every.professor,
+                        every.cap, every.time, str(i[every]), "scheduled"]
+                output_list.append(temp)
+
+    # adding unscheduled courses to the output and alt info list as strings
+    for i in courses:
+        if (not (i.shed)):
+            if str(i.ver) == 'nan':
+                version = ""
+            else:
+                version = i.ver
+        
+            temp = [i.subject + " " + str(i.course), i.title, version, i.sec, i.professor, i.cap, i.time, "",
+                    "unscheduled"]
+            output_list.append(temp)
+
+            temp = [i.subject + " " + str(i.course) + i.title + str(i.sec) + i.professor]
+            alt_list.append(temp)
+
+
+    # writing to output excel workbook file that the user specifies as the second argument
+    out_workbook = xlsxwriter.Workbook(sys.argv[2])
+    # create sheet and header schedule
+    scheduleSheet = out_workbook.add_worksheet('Schedule')
+    Sheader = ["Course", "Title", "Version", "Section", "Professor", "Capacity", "Time", "Room", "Status"]
+    # add schedule
+    for i in range(len(output_list)):
+        for j in range(len(output_list[i])):
+            if i == 0:
+                scheduleSheet.write(i, j, Sheader[j])
+
+            else:
+                scheduleSheet.write(i, j, output_list[i][j])
+
+    # create alternatives sheet and header
+    altSheet = out_workbook.add_worksheet('Alternatives')
+    Aheader = ["course", "alt1", "alt2", "alt3"]
+
+    # add alternatives
+    for i in range(len(alt_list)):
+        for j in range(len(Aheader)):
+            if i == 0:
+                altSheet.write(i, j, Aheader[j])
+
+            elif j == 0:
+
+                altSheet.write(i, j, alt_list[i][j])
+            else:
+
+                if len(spring2020.unScheduled[i].alt) != 0:
+                    altSheet.write(i,j, str(spring2020.unScheduled[i].alt[j-1]))
+                else:
+                    altSheet.write(i, j,"No alternatives")
+
+    # close workbook
+    out_workbook.close()
 
 
 # use inline command holding the file name
@@ -80,15 +158,15 @@ for i in dataClasses.Time:
 
     # if statement to separate slots based on days and to avoid repeats
     if (("mw" in i) or ("MW" in i)) and not (i in spring2020.mw):
-        spring2020.mw.append(i)
+        spring2020.mw.append(i.lower())
     if (("tt" in i) or ("TT" in i)) and not (i in spring2020.tt):
-        spring2020.tt.append(i)
+        spring2020.tt.append(i.lower())
     if (("MWF" in i) or ("mwf" in i)) and not (i in spring2020.mwf):
-        spring2020.mwf.append(i)
+        spring2020.mwf.append(i.lower())
 
 # create courses and add to Course list
 for i in courses:
-    courseList.append(Course(i[0], i[1], i[2], i[3], i[4], i[5], i[6], i[7]))
+    courseList.append(Course(i[0], i[1], i[2], i[3], i[4], i[5], i[6].lower(), i[7]))
 
 # create rooms and add to room list
 for i in rooms:
@@ -102,7 +180,7 @@ for i in spring2020.mw:
     # loop all courses
     for j in courseList:
         # if the course time is for monday/wednesday or monday/wednesday/friday (checking for upper and lower case)
-        if (j.time == i) or (j.time.upper() == i) or ("MWF" in j.time) or ("mwf" in j.time):
+        if j.time == i or "mwf" in j.time:
             # loop through rooms
             for k in roomList:
                 # if the rooms cap is suitable and the room is not taken for the time slot and the course is not scheduled
@@ -116,7 +194,7 @@ for i in spring2020.mw:
                     spring2020.solution[0].append({j: k})
                     spring2020.solution[2].append({j: k})
                     # if course is also scheduled for friday add to solution list for friday
-                    if ("MWF" in j.time) or ("mwf" in j.time):
+                    if "mwf" in j.time:
                         k.hours += 1.25
                         spring2020.solution[4].append({j: k})
     # loop over all rooms and reset taken value for next time slot
@@ -128,7 +206,7 @@ for i in spring2020.mw:
 # same procedure as above loop but for time slots available tuesday and thursday
 for i in spring2020.tt:
     for j in courseList:
-        if (j.time == i) or (j.time.upper() == i):
+        if j.time == i:
             for k in roomList:
                 if (j.cap <= k.cap) and not (k.taken) and not (j.shed):
                     j.shed = True
@@ -140,6 +218,10 @@ for i in spring2020.tt:
         if not t.taken:
             spring2020.freeSlots.append({i: t})
         t.taken = False
+
+for i in courseList:
+    if not i.shed:
+        spring2020.unScheduled.append(i)
 
 # print solution list for mondays
 print("Monday:")
@@ -164,35 +246,25 @@ for i in spring2020.solution[4]:
 # print any unscheduled courses
 print("Unscheduled:")
 # loop over course list and check shed bool
-for i in courseList:
-    if not i.shed:
-        print(i)
+for i in spring2020.unScheduled:
+    print(i)
 
-# making a list that formats output info
-output_list = []
-for solution in spring2020.solution:
-    for i in solution:
-        for every in i.keys():
-            if str(every.ver) == 'nan':
-                version = ""
-            else:
-                version = str(every.ver)
-            line = str(every.subject) + " " + str(every.course) + "," + str(every.title) + "," + version + "," + str(
-                every.sec) + ",\"" + every.professor + "\"," + str(every.cap) + "," + every.time + ",\"" + str(
-                i[every]) + "\",scheduled"
 
-            output_list.append(line)
-# adding unscheduled courses to the output info list
-for i in courseList:
-    if not i.shed:
-        line = str(every.subject) + " " + str(every.course) + "," + str(every.title) + "," + str(every.ver) + "," + str(
-            every.sec) + ",\"" + every.professor + "\"," + str(every.cap) + "," + every.time + "," + ",unscheduled"
-        output_list.append(line)
-# writing to output csv file that the user specifies as the second argument
-outfile = open(sys.argv[2], 'w')
-outfile.write("Course,Title,Version,Section,Professor,Capacity,Time,Room,Status")
-outfile.write('\n')
-for i in output_list:
-    outfile.write(i)
-    outfile.write('\n')
-outfile.close()
+# loop over unscheduled classes
+for course in spring2020.unScheduled:
+    # loop over available classes
+    for slots in spring2020.freeSlots:
+        # for time slots in free spaces
+        for keys in slots:
+            # try and fit into the same days as professor originally asked for
+            if course.time == keys and course.cap <= slots[keys].cap:
+                course.alt.append(slots)
+            # if not fit into first slot found
+            elif slots[keys].cap >= course.cap:
+                course.alt.append(slots)
+        # limit to 3 alternatives max
+        if len(course.alt) > 2:
+            break
+
+
+generate_output(spring2020, courseList)
--- PythonScheduler/scheduler_gui.py
+++ PythonScheduler/scheduler_gui.py
@@ -1,67 +1,138 @@
-#from tkinter import *
+# from tkinter import *
 import os
 import tkinter as tk
 import subprocess as sub
 import sys
+# import xlrd
+import pandas as pd
+
 global G_out
-PIPE ='|'
+global inFile
+global outFile
+PIPE = '|'
+try:
+    from Tkinter import Canvas
+    from Tkconstants import *
+except ImportError:
+    from tkinter import Canvas
+    from tkinter.constants import *
+
+from PIL import Image, ImageDraw, ImageTk
+
+# Python 2/3 compatibility
+try:
+    basestring
+except NameError:
+    basestring = str
+
+"""def hex2rgb(str_rgb):
+    try:
+        rgb = str_rgb[1:]
+        if len(rgb) == 6:
+            r, g, b = rgb[0:2], rgb[2:4], rgb[4:6]
+        elif len(rgb) == 3:
+            r, g, b = rgb[0] * 2, rgb[1] * 2, rgb[2] * 2
+        else:
+            raise ValueError()
+    except:
+        raise ValueError("Invalid value %r provided for rgb color."% str_rgb)
+    return tuple(int(v, 16) for v in (r, g, b))
+"""
+
 
 class Page(tk.Frame):
     def __init__(self, *args, **kwargs):
-        tk.Frame.__init__(self, *args, **kwargs)
+        tk.Frame.__init__(self, *args, **kwargs, bg='blue')
+
     def show(self):
         self.lift()
 
+
 class Page1(Page):
-   def __init__(self, *args, **kwargs):
-       Page.__init__(self, *args, **kwargs)
-       label = tk.Label(self, text="Welcome to the Schedule Generator!")
-       label.pack(side="top", fill="both", expand=True)
+    def __init__(self, *args, **kwargs):
+        Page.__init__(self, *args, **kwargs)
+        label = tk.Label(self, text="Welcome to the Schedule Generator!")
+        label.pack(side="top", fill="both", expand=True)
+
+
 def generate():
-    #os.system('python3 schedule2.py ClassRoom.xlsx output.csv')
+    # os.system('python3 schedule2.py ClassRoom.xlsx output.csv')
     global G_out
-    output = sub.run(['python3', 'schedule2.py', 'ClassRoom.xlsx', 'output.csv'], check=True, capture_output = True).stdout
+    output = sub.run(['python3', 'schedule2.py', inFile, outFile], check=True, capture_output=True).stdout
     output = str(output)[2:]
     G_out = output.split('\\n')
-    #x = sub.check_output(['python3','schedule2.py','ClassRoom.xlsx','output.csv'])
-    #sys.stdout = Std_redirector(text)
-    #p = sub.Popen('python3 schedule2.py ClassRoom.xlsx output.csv',stdout=sub.PIPE,stderr=sub.PIPE)
-    #output, errors = p.communicate()
-    
+    # x = sub.check_output(['python3','schedule2.py','ClassRoom.xlsx','output.csv'])
+    # sys.stdout = Std_redirector(text)
+    # p = sub.Popen('python3 schedule2.py ClassRoom.xlsx output.csv',stdout=sub.PIPE,stderr=sub.PIPE)
+    # output, errors = p.communicate()
+
+
 def view_schedule():
     global G_out
     master = tk.Tk()
     scrollbar = tk.Scrollbar(master)
-    scrollbar.pack(side="right", fill = tk.Y, expand=False)
+    scrollbar.pack(side="right", fill=tk.Y, expand=False)
     listbox = tk.Listbox(master, yscrollcommand=scrollbar.set)
-    for i in range(len(G_out)):
-        listbox.insert(tk.END, str(G_out[i]))
-        #listbox.insert(tk.END, '\n')
-    listbox.pack(side="left", fill=tk.BOTH,expand=True)
+    df = pd.read_excel(outFile, sheet_name='Schedule')
+    sched_list = df.values.tolist()
+    print_out_list = {"Scheduled:": [], "Unscheduled:": []}
+
+    for i in range(len(sched_list)):
+        if sched_list[i][len(sched_list[i]) - 1]:
+            print_out_list["Scheduled:"].append(sched_list[i][:len(sched_list[i]) - 1])
+        else:
+            print_out_list["Unscheduled:"].append(sched_list[i][:len(sched_list[i]) - 1])
+    for key in print_out_list.keys():
+        listbox.insert(tk.END, key)
+        for i in range(len(print_out_list[key])):
+            listbox.insert(tk.END, str(print_out_list[key][i]))
+        # listbox.insert(tk.END, '\n')
+    listbox.pack(side="left", fill=tk.BOTH, expand=True)
 
     scrollbar.config(command=listbox.yview_scroll)
-    
-    temp = tk.Button( text="test")
-    outputfile = open("output.csv")
-    
+
+    temp = tk.Button(text="test")
+    outputfile = open("output.xlsx")
+
     outputfile.close()
     master.mainloop()
 
+
 class Page2(Page):
-   def __init__(self, *args, **kwargs):
-       Page.__init__(self, *args, **kwargs)
-       label = tk.Label(self, text="Show schedule here")
-       label.pack(side="top", fill="both", expand=True)
+    def __init__(self, *args, **kwargs):
+        Page.__init__(self, *args, **kwargs)
+        label = tk.Label(self, text="Show schedule here")
+        label.pack(side="top", fill="both", expand=True)
+
 
 class Page3(Page):
-   def __init__(self, *args, **kwargs):
-       Page.__init__(self, *args, **kwargs)
-       label = tk.Label(self, text="Show statistics here")
-       label.pack(side="top", fill="both", expand=True)
-        
+    def __init__(self, *args, **kwargs):
+        Page.__init__(self, *args, **kwargs)
+        label = tk.Label(self, text="Show statistics here")
+        label.pack(side="top", fill="both", expand=True)
+
+
+def go(page, inF, outF):
+    global inFile
+    global outFile
+    # do error checking for input and output files here
+    inFile = str(inF.get())
+    outFile = str(outF.get())
+    if "xlsx" in inF.get() and "xlsx" in outF.get():  # Checks whether input and output files are correctly formatted
+
+        root.deiconify()  # Unhides the root window
+        page.destroy()  # Removes the toplevel window
+
+
+def quit_top(top, root):
+    top.destroy()  # Removes the toplevel window
+    root.destroy()  # Removes the hidden root window
+    sys.exit()  # Ends the script
+
+
 class MainView(tk.Frame):
     def __init__(self, *args, **kwargs):
-        tk.Frame.__init__(self, *args, **kwargs)
+        tk.Frame.__init__(self, *args, **kwargs, bg='blue')
         p1 = Page1(self)
         p2 = Page2(self)
         p3 = Page3(self)
@@ -85,38 +156,87 @@ class MainView(tk.Frame):
 
         p1.show()
 
+
+'''class GradientFrame(Canvas):
+    def __init__(self, master, from_color, to_color, width=None, height=None, orient=HORIZONTAL, steps=None, **kwargs):
+        Canvas.__init__(self, master, **kwargs)
+        if steps is None:
+            if orient == HORIZONTAL:
+                steps = height
+            else:
+                steps = width
+        if isinstance(from_color, basestring):
+            from_color = hex2rgb(from_color)
+
+        if isinstance(to_color, basestring):
+            to_color = hex2rgb(to_color)
+        r,g,b = from_color
+        dr = float(to_color[0] - r)/steps
+        dg = float(to_color[1] - g)/steps
+        db = float(to_color[2] - b)/steps
+        if orient == HORIZONTAL:
+            if height is None:
+                raise ValueError("height can not be None")
+
+            self.configure(height=height)
+
+            if width is not None:
+                self.configure(width=width)
+            img_height = height
+            img_width = self.winfo_screenwidth()
+            image = Image.new("RGB", (img_width, img_height), "#FFFFFF")
+            draw = ImageDraw.Draw(image)
+            for i in range(steps):
+                r,g,b = r+dr, g+dg, b+db
+                y0 = int(float(img_height * i)/steps)
+                y1 = int(float(img_height * (i+1))/steps)
+                draw.rectangle((0, y0, img_width, y1), fill=(int(r),int(g),int(b)))
+        else:
+            if width is None:
+                raise ValueError("width can not be None")
+            self.configure(width=width)
+
+            if height is not None:
+                self.configure(height=height)
+            img_height = self.winfo_screenheight()
+            img_width = width
+
+            image = Image.new("RGB", (img_width, img_height), "#FFFFFF")
+            draw = ImageDraw.Draw(image)
+            for i in range(steps):
+                r,g,b = r+dr, g+dg, b+db
+                x0 = int(float(img_width * i)/steps)
+                x1 = int(float(img_width * (i+1))/steps)
+                draw.rectangle((x0, 0, x1, img_height), fill=(int(r),int(g),int(b)))
+
+        self._gradient_photoimage = ImageTk.PhotoImage(image)
+        self.create_image(0, 0, anchor=NW, image=self._gradient_photoimage)
+'''
 if __name__ == "__main__":
     root = tk.Tk()
+    root.title('Main Menu')
+    entry = tk.Toplevel(bg='#99badd')
+    label1 = tk.Label(entry, text="Input file:", bg='#99badd').pack(side='left')
+    inputFile = tk.Entry(entry)
+    inputFile.config(highlightthickness=0)
+    inputFile.pack(side='left')
+    label2 = tk.Label(entry, text="Output file:", bg='#99badd').pack(side='left')
+    outputFile = tk.Entry(entry)
+    outputFile.config(highlightthickness=0)
+    outputFile.pack(side='left')
+    button1 = tk.Button(entry, text="Go", command=lambda: go(entry, inputFile, outputFile), bg='black',
+                        fg="white")  # keep going button
+    button1.config(highlightthickness=0)
+    button2 = tk.Button(entry, text="Quit", command=lambda: quit_top(entry, root), bg='black',
+                        fg='white')  # quit button
+    button2.config(highlightthickness=0)
+    # epage = EntryPage(entry)
+    button1.pack(side='left')
+    button2.pack(side='left')
+    root.withdraw()
+    # bg_frame = GradientFrame(root, from_color="#000000", to_color="#E74C3C", height=100)
+
     main = MainView(root)
-    main.pack(side="top", fill="both" ,expand=True)
+    main.pack(side="top", fill="both", expand=True)
     root.wm_geometry("400x400")
     root.mainloop()
-'''
-
-def generate():
-    os.system('python3 schedule2.py ClassRoom.xlsx output.csv')
-    
-root = Tk()
-frame = Frame(root)
-frame.pack()
-
-root.geometry("500x100")
-
-bottomframe = Frame(root)
-bottomframe.pack( side = BOTTOM )
-
- 
-
-generatebutton = Button(frame, text = "Generate Schedule", fg = "black", command=generate)
-generatebutton.pack()# side = LEFT)
-
- 
-
-viewbutton = Button(frame, text = "View Schedule", fg = "black")
-viewbutton.pack()# side = RIGHT )
-
-
- 
-
-root.mainloop()
-'''
